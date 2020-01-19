e9483ab97482d1185fa0605804e8309f1db6bfe9
KoMinjae <alswovv@naver.com>
2020-01-19 15:57:27 +0900

    SLPROJECT 최종본

--- app/src/main/java/com/example/slproject/SLActivity.java
+++ app/src/main/java/com/example/slproject/SLActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,10 @@ public class SLActivity extends AppCompatActivity {
     TextView textView3;
     TextView textView2;
     float density;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -26,17 +32,44 @@ public class SLActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sl);
         textView1 = (TextView)findViewById(R.id.sltext1);
         textView2 = (TextView)findViewById(R.id.sltext2);
-        textView3 = (TextView)findViewById(R.id.sltext3);
-
+        imageView1 = (ImageView)findViewById(R.id.imageView1);
+        imageView2 = (ImageView)findViewById(R.id.imageView2);
+        imageView3 = (ImageView)findViewById(R.id.imageView3);
+        imageView4 = (ImageView)findViewById(R.id.imageView4);
+        imageView4.setVisibility(View.GONE);
+        imageView3.setVisibility(View.GONE);
         textView1.setText("해당 어플리케이션은 수어를 모르는 사람이 수어를 통하여 청각장애인과 소통을 돕기 위해 제작되었습니다. 또한 수어를 배우고자 하는 사람도 유용하게 사용 가능합니다.");
         textView2.setText("카카오 음성 API와 한글형태소 분석기 KOMORAN을 사용하였습니다.");
 
         textView1.setTextSize((float) (getStandardSizeX()/80 ));
         textView2.setTextSize((float) (getStandardSizeX()/80 ));
-        textView3.setTextSize((float) (getStandardSizeX()/30 ));
         textView1.setTextSize((float) (getStandardSizeY()/45));
         textView2.setTextSize((float) (getStandardSizeY()/45));
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView3.setVisibility(View.VISIBLE);
+            }
+        });
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView3.setVisibility(View.GONE);
+            }
+        });
+        imageView2.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView4.setVisibility(View.VISIBLE);
+            }
+        }));
+        imageView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView4.setVisibility(View.GONE);
 
+            }
+        });
         bottomNavigationView = findViewById(R.id.bottomNavi);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
